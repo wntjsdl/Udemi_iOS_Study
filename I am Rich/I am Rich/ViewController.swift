73319b30cff491b27da3152c17e9dd7a2f8f7db1
Jusun Kang <wntjs0305@cocen.com>
2022-12-16 17:23:13 +0900

    create first project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by powerapp on 2022/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
