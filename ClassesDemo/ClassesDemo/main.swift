b7fbd2a678ae7187b965c958953fccd8eb61bcbb
Jusun Kang <wntjs0305@cocen.com>
2023-01-30 01:14:45 +0900

    Section 11: Swift Deep Dive

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,12 @@
+
+let skeleton = Enemy()
+print(skeleton.health)
+skeleton.move()
+skeleton.attack()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "My teeth are swords! My clasw are spears! My wings are a hurricane")
+dragon.move()
+dragon.attack()
